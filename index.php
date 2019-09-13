cd92006d7904cdc4738324c837c83c8fe11d56b2
aluno <aluno>
2019-09-13 15:38:25 -0300

    Fase 2

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+	<title>Contador App</title>
+
+</head>
+<body>
+	
+	<header>
+		<h1>Contador de coisas</h1>
+	</header>
+
+	<main>
+		<div id="container">
+			<div class="contador">
+				<button class="bot">⊖</button>
+				<div>
+					<p class="nome">xixi</p>
+					<p class="num">2</p>
+				</div>
+				<button class="bot">⊕</button>
+				
+			</div>
+			<div class="contador">
+				<button class="bot">⊖</button>
+				<div>
+					<p class="nome">Copos d'água</p>
+					<p class="num">4</p>
+				</div>
+				<button class="bot">⊕</button>
+				
+			</div>
+			<div class="contador">
+				<button class="bot">⊖</button>
+				<div>
+					<p class="nome">Biscoitos</p>
+					<p class="num">7</p>
+				</div>
+				<button class="bot">⊕</button>
+				
+			</div>
+		</div>
+	</main>
+	<hr>
+	<footer>
+		<form action="index.php" method="">
+			<p><label>Novo Contador</label></p>
+			<input type="text" name="ncontador">
+			<button id="ad">Adicionar</button>
+		</form>
+	</footer>
+	
+
+
+</body>
+</html>
